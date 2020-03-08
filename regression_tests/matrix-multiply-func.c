bede1a90b6aa46e44132552df7fb967dc67e9f6a
santosh <santosh.nagarakatte@gmail.com>
2020-03-08 19:38:42 -0400

    adding microbenchmarks to study performance impact of various design choices in fpsan

---
+++ regression_tests/matrix-multiply-func.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+/* fpsanitizer: A micro benchmark to test performance overheads with
+   frequent mallocs and frees and to measure the cost of function
+   calls and return with FP arguments */
+
+
+float mult_ind(float a, float b){
+  return a * b;
+}
+
+float mult_add(float a, float b){
+  return a + b;
+}
+
+float** matrix_mult(float** A, float** B,
+		    int m1, int n1, int m2, int n2){
+  
+  assert (n1 == m2);
+  float** result = malloc(m1 * sizeof(float *));
+
+  for(int i =0 ; i < m1; i++){
+    result[i] = malloc(n2 * sizeof(float));
+    for(int j = 0; j < n2; j++){
+      result[i][j] = 0.0f;
+    }
+  }
+
+  for(int i =0; i < m1; i++){
+    for(int j= 0; j< n2; j++){
+      result[i][j] = 0;
+      for(int k = 0; k < n1; k++){
+
+	float t1 = mult_ind(A[i][k], B[k][j]);
+	result[i][j] = mult_add(result[i][j], t1);
+      }
+    }
+  }
+  return result;
+}
+
+float** allocate_matrix(int m1, int n1){
+
+  float ** matrix;
+  matrix = malloc(m1 * sizeof(float *));
+  for(int i = 0; i< m1; i++){
+    matrix[i] = malloc(n1 * sizeof(float));
+  }  
+  return matrix;
+}
+void print_free(float** result, int m1, int n1){
+
+#ifdef PRINT_RESULT  
+  for(int i = 0; i< m1; i++){
+    for(int j = 0; j < n1; j++){
+      printf("%f ", result[i][j]);
+    }
+    printf("\n");
+  }
+#endif
+
+
+  for(int i = 0; i < m1; i++){
+    free(result[i]);
+  }
+  free(result);
+}
+
+int main(int argc, char** argv){
+
+  char* filename = "test.txt";
+  int m1, n1, m2, n2;
+
+  int iterations = atoi(argv[1]);
+
+  FILE* fp = fopen(filename, "r");
+  assert (fp != NULL);
+  fscanf(fp, "%d %d %d %d\n", &m1, &n1, &m2, &n2);
+
+  float** A = allocate_matrix(m1, n1);
+  float** B = allocate_matrix(m2, n2);
+
+  for(int i = 0; i < m1; i++){
+    for(int j = 0; j < n1; j++){
+      fscanf(fp, "%f", &A[i][j]);
+    }
+    fscanf(fp, "\n");
+  }
+
+  for(int i = 0; i < m2; i++){
+    for(int j = 0; j < n2; j++){
+      fscanf(fp, "%f", &B[i][j]);
+    }
+    fscanf(fp, "\n");
+  }
+
+  for(int i = 0; i < iterations; i++){
+    float** result = matrix_mult(A,B, m1, n1, m2, n2);
+    print_free(result, m1, n2);
+  }  
+ 
+  return 0;
+}
